a46c80d5a42558e1af0873dcc067c5902c2c2fca
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-07 15:14:57 +0100

    Creates the members and activities table in gym.sql

---
+++ db/gym.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS members;
+DROP TABLE IF EXISTS activities;
+
+CREATE TABLE members (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255)
+);
+
+CREATE TABLE activities (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    time VARCHAR(255)
+);
--- models/activity.py
+++ models/activity.py
@@ -1,5 +1,5 @@
 class Activity:
-    def __init__(self, name, time, id=None):
+    def __init__(self, name, day_of_week, time, id=None):
         self.name = name
         self.time = time
         self.id = id
