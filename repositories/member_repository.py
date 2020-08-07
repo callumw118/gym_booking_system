bacd8f7d41a2ddff13f223422ec6b748e37ce74e
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-07 16:13:49 +0100

    Adds select_all members method

--- repositories/member_repository.py
+++ repositories/member_repository.py
@@ -8,6 +8,18 @@ def save(member):
     id = results[0]['id']
     member.id = id
 
+
 def delete_all():
     sql = "DELETE FROM members"
     run_sql(sql)
+
+
+def select_all():
+    members = []
+    sql = "SELECT * FROM members"
+    results = run_sql(sql)
+
+    for result in results:
+        member = Member(result['full_name'], result['id'])
+        members.append(member)
+    return members
