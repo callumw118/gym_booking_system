9a8970074495e4c01a41e5df54c9cc3003c0fc9b
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-07 19:52:28 +0100

    Adds save booking function

--- repositories/booking_repository.py
+++ repositories/booking_repository.py
@@ -5,3 +5,11 @@ from models.activity import Activity
 
 import repositories.member_repository as member_repository
 import repositories.activity_repository as activity_repository
+
+
+def save(booking):
+    sql = "INSERT INTO bookings (member_id, activity_id) VALUES (%s, %s) RETURNING id"
+    values = [booking.member.id, booking.activity.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    booking.id = id
