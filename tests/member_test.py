5e431299ed5622f8e0cd16595ab8430c54d0de39
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-07 14:45:43 +0100

    Tests for member name function

--- tests/member_test.py
+++ tests/member_test.py
@@ -5,3 +5,6 @@ class TestMember(unittest.TestCase):
 
     def setUp(self):
         self.member = Member("Callum Wolfe")
+
+    def test_member_has_name(self):
+        self.assertEqual("Callum Wolfe", self.member.full_name)
