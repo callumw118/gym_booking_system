61d161bb87b4f5a6681f48cb7a33991c09d8f99c
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-07 15:16:45 +0100

    Adds test for day of week for the activity

--- tests/activity_test.py
+++ tests/activity_test.py
@@ -4,10 +4,13 @@ from models.activity import Activity
 class TestActivity(unittest.TestCase):
 
     def setUp(self):
-        self.activity = Activity("HIIT (High Intensity Training)", "13:00")
+        self.activity = Activity("HIIT (High Intensity Training)", "Monday", "13:00")
 
     def test_activity_has_name(self):
         self.assertEqual("HIIT (High Intensity Training)", self.activity.name)
+
+    def test_activity_has_day_of_week(self):
+        self.assertEqual("Monday", self.activity.day_of_week)
 
     def test_activity_has_time(self):
         self.assertEqual("13:00", self.activity.time)
