29fa426673dfdd2774c7a7f4216a04845ae36a4d
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-07 16:15:00 +0100

    Adds another member to database to test if select_all method works, which it does

--- console.py
+++ console.py
@@ -8,4 +8,7 @@ member_repository.delete_all()
 member_1 = Member("Callum Wolfe")
 member_repository.save(member_1)
 
+member_2 = Member("James Mann")
+member_repository.save(member_2)
+
 pdb.set_trace()
