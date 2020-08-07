ad2ca3fd71574bb377693d4b9fc87e2586c02ef6
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-07 15:26:22 +0100

    Fixes typo in run_sql file

--- db/run_sql.py
+++ db/run_sql.py
@@ -1,16 +1,17 @@
-import psycopg2
+import psycopg2  
 import psycopg2.extras as ext
 
 def run_sql(sql, values = None):
-    results = []
     conn = None
+    results = []
+    
     try:
-        conn = psycopg2.connect("dbname = 'gym'")
-        cur = conn.cursor(cursor_factory=psycopg2.ext.Dictcursor)
+        conn=psycopg2.connect("dbname='gym'")
+        cur = conn.cursor(cursor_factory=ext.DictCursor)   
         cur.execute(sql, values)
         conn.commit()
         results = cur.fetchall()
-        cur.close()
+        cur.close()           
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
