3d131c93afd0b5b51be261b399f1f804acf17079
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-07 15:57:22 +0100

    Adds save member function to member_repository

--- repositories/member_repository.py
+++ repositories/member_repository.py
@@ -1,3 +1,9 @@
 from db.run_sql import run_sql
 from models.member import Member
 
+def save(member):
+    sql = 'INSERT INTO members (full_name) VALUES (%s) RETURNING id'
+    values = [member.full_name]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    member.id = id
