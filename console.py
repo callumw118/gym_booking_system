930f096916cf88b9e90028a6baf15faee0642e9c
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-07 16:01:14 +0100

    Console runs with member being saved to database. Save function working and delete_all working as intended

--- console.py
+++ console.py
@@ -1,1 +1,11 @@
 import pdb
+
+from models.member import Member
+import repositories.member_repository as member_repository
+
+member_repository.delete_all()
+
+member_1 = Member("Callum Wolfe")
+member_repository.save(member_1)
+
+pdb.set_trace()
