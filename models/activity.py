53dbafc2a19bbb5e169f7509c26f57bfe54241e5
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-07 15:15:25 +0100

    Adds day of week to activity model

--- models/activity.py
+++ models/activity.py
@@ -1,5 +1,6 @@
 class Activity:
     def __init__(self, name, day_of_week, time, id=None):
         self.name = name
+        self.day_of_week = day_of_week
         self.time = time
         self.id = id
