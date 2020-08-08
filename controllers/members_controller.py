20275e5173a14a2489a3d8ab5db1d865c69ae2c7
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-08 11:52:43 +0100

    Adds add new member route with ability to add a new member to database. Redirects to members page with updated list of members

--- controllers/members_controller.py
+++ controllers/members_controller.py
@@ -16,3 +16,12 @@ def members():
 @members_blueprint.route("/members/new")
 def new_member():
     return render_template("members/new.html")
+
+
+# CREATE
+@members_blueprint.route("/members", methods=["POST"])
+def create_member():
+    full_name = request.form["full_name"]
+    new_member = Member(full_name)
+    member_repository.save(new_member)
+    return redirect("/members")
