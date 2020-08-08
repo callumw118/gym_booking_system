e2c742a20f6a0a37c9f8deaa9579e46627e18e1e
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-08 11:42:05 +0100

    Adds index route for members which displays all members saved in database

--- controllers/members_controller.py
+++ controllers/members_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Flash, redirect, render_template, request
+from flask import Blueprint, Flask, redirect, render_template, request
 
 from models.member import Member
 import repositories.member_repository as member_repository
@@ -10,3 +10,5 @@ members_blueprint = Blueprint("members", __name__)
 def members():
     members = member_repository.select_all()
     return render_template("members/index.html", members=members)
+
+
