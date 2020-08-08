583f43fa4053bd617b74aca9679dc9a07d010ef8
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-08 12:13:05 +0100

    Adds index route method to show all activities

--- controllers/activities_controller.py
+++ controllers/activities_controller.py
@@ -5,3 +5,9 @@ import repositories.activity_repository as activity_repository
 
 activities_blueprint = Blueprint("activities", __name__)
 
+
+# INDEX
+@activities_blueprint.route("/activities")
+def activities():
+    activities = activity_repository.select_all()
+    return render_template("/activities/index.html", activities=activities)
