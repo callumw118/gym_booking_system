bc4dd2a53122ffd9e2ff306ddf78c271db91164a
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-09 11:20:18 +0100

    Adds capacity to activity model

--- models/activity.py
+++ models/activity.py
@@ -1,6 +1,7 @@
 class Activity:
-    def __init__(self, name, day_of_week, time, id=None):
+    def __init__(self, name, day_of_week, time, capacity, id=None):
         self.name = name
         self.day_of_week = day_of_week
         self.time = time
+        self.capacity = capacity
         self.id = id
