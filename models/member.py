c351739b92176bcf067a9c8f719d59d19602fdcd
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-10 15:27:28 +0100

    Adds status to member, sets to active as default

--- models/member.py
+++ models/member.py
@@ -1,5 +1,6 @@
 class Member:
-    def __init__(self, full_name, membership, id=None):
+    def __init__(self, full_name, membership, status="Active", id=None):
         self.full_name = full_name
         self.membership = membership
+        self.status = status
         self.id = id
