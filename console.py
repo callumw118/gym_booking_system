182cf0d8617492b52767100c55dd72255ad178cd
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-11 16:05:47 +0100

    Changes color of text and member card

--- console.py
+++ console.py
@@ -19,11 +19,14 @@ member_repository.save(member_1)
 member_2 = Member("James Mann", "Premium")
 member_repository.save(member_2)
 
-activity_1 = Activity("HIIT (High Intensity Training)", "Monday", "13:00", 3)
+activity_1 = Activity("HIIT (High Intensity Training)", "Monday", "13:00", 10)
 activity_repository.save(activity_1)
 
 activity_2 = Activity("Indoor Cycling", "Monday", "15:00", 20)
 activity_repository.save(activity_2)
+
+activity_3 = Activity("Functional Strength", "Tuesday", "18:00", 12)
+activity_repository.save(activity_3)
 
 booking_1 = Booking(member_1, activity_1)
 booking_repository.save(booking_1)
