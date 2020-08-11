4b903a528dd3772a09c106bddff9de62262ea8dc
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-11 17:02:17 +0100

    Adds space between to container property for activies

--- console.py
+++ console.py
@@ -16,10 +16,10 @@ activity_repository.delete_all()
 member_1 = Member("Callum Wolfe", "Premium")
 member_repository.save(member_1)
 
-member_2 = Member("James Mann", "Premium")
+member_2 = Member("James Mann", "Standard")
 member_repository.save(member_2)
 
-activity_1 = Activity("HIIT (High Intensity Training)", "Monday", "13:00", 10)
+activity_1 = Activity("BoxFit", "Monday", "13:00", 10)
 activity_repository.save(activity_1)
 
 activity_2 = Activity("Indoor Cycling", "Monday", "15:00", 20)
