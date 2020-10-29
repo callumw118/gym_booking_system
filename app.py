6043eb289695f6ea3206aad6a331e97f075fa5ca
Callum Wolfe <wolfe.callum@gmail.com>
2020-10-29 12:59:04 +0000

    remove sqlalchemy

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_heroku import Heroku
+# from flask_sqlalchemy import SQLAlchemy
 
 from controllers.members_controller import members_blueprint
 from controllers.activities_controller import activities_blueprint
 from controllers.bookings_controller import bookings_blueprint
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
 app.register_blueprint(members_blueprint)
 app.register_blueprint(activities_blueprint)
